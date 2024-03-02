76da9224af9b51deff45109cdeb26ab001479c4c
iqbalpa <iqbalpahlevi16@gmail.com>
2024-03-02 21:51:23 +0700

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dashboard
+
+This is a simple dashboard to display the data from the [day.csv](../data/day.csv) file.
+
+## Link to the dashboard
+
---
+++ dashboard/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Hide PyplotGlobalUseWarning
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+data = pd.read_csv('dashboard/main_data.csv')
+
+st.title("The most popular day for group of users")
+
+day_grouped = data.groupby('weekday')
+casual_by_weekday = day_grouped['casual'].sum()
+registered_by_weekday = day_grouped['registered'].sum()
+
+selected_feature = st.selectbox('Select type of users', ['casual', 'registered'])
+
+submit = st.button('Show')
+
+if submit:
+    plt.figure(figsize=(10, 6))
+    if selected_feature == 'casual':
+        plt.bar(casual_by_weekday.index, casual_by_weekday.values)
+        plt.title('Casual users by weekday')
+    else:
+        plt.bar(registered_by_weekday.index, registered_by_weekday.values)
+        plt.title('Registered users by weekday')
+    st.pyplot()
+
